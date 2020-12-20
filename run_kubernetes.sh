2df883f8e4201c471e9d735ab4ee0aa1cf12b3c0
Manu <Manu@bagile.co.uk>
2020-12-20 21:43:14 +0300

    Kubernetes output updated

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="pmanu1977/udacity_proj4"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-proj4\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=udacity-proj4
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity-proj4  8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="pmanu1977/udacity_proj4"
 
 # Step 2:  
 # Authenticate & tag
+docker login --username "pmanu1977" && docker image tag udacity_proj4 $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
